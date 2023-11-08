6fa79abfa73653263b8d8e359c2e70fcdd019550
Rajiv Kumawat <rajivkumawat28012002@gmail.com>
2023-11-08 23:26:09 +0530

    second-commit

--- backend/models/Attendence.js
+++ backend/models/Attendence.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const attendanceSchema = new mongoose.Schema({
     student: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Student',
+        ref: Student,
         required: true
     },
     date: {
